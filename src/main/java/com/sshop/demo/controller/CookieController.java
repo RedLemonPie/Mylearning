0aa873aeb0af86f02f3323fb6596f2894782a5e4
unknown <019576@sss.com>
2018-12-28 17:17:38 +0800

    28 add权限管理andsession

---
+++ src/main/java/com/sshop/demo/controller/CookieController.java
@@ -0,0 +1,39 @@
+package com.sshop.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Cookie;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class CookieController {
+    @RequestMapping("/cookie")
+    public Map<String, Object> cookie(@RequestParam("browser") String browser, HttpServletRequest request, HttpSession session) {
+        //取出session中的browser
+        Object sessionBrowser = session.getAttribute("browser");
+        Map<String, Object> map = new HashMap<String,Object>();
+        if (sessionBrowser == null) {
+            System.out.println("不存在session，设置browser=" + browser);
+            session.setAttribute("browser", browser);
+            map.put("browser", browser);
+        } else {
+            System.out.println("存在session，browser=" + sessionBrowser.toString());
+        }
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                System.out.println(cookie.getName() + " : " + cookie.getValue());
+                map.put("cookiename", cookie.getName());
+                map.put("cookievalues", cookie.getValue());
+            }
+        }
+        return map;
+    }
+}
--- src/main/java/com/sshop/demo/model/bean/UserDTO.java
+++ src/main/java/com/sshop/demo/model/bean/UserDTO.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 @Data
 public class UserDTO implements Serializable {
 
+    private static final long serialVersionUID = -526324944915280489L;
     private Integer id;
     private Integer score;
 
